1e7362672a73fd99e4c7874c2925f703d8f31ac1
Jonathan Berg Hjelm <jonathan.hjelm@yahoo.dk>
2017-05-23 16:19:40 +0200

    Made the start of the SQLITE Database

---
+++ app/src/main/java/Model/ConnHomeContract.java
@@ -0,0 +1,74 @@
+package Model;
+
+import android.bluetooth.BluetoothClass;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Jonathan on 23-05-2017.
+ */
+
+public final class ConnHomeContract {
+    // To prevent someone from acidentally instantiating the contract class.
+    private ConnHomeContract() {}
+
+    public static class DeviceEntry implements BaseColumns {
+        public static final String TABLE_NAME = "devices";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_TYPEID = "type";
+        public static final String COLUMN_NAME_PINNUMBER = "pinnumber";
+    }
+
+    public static class TypeEntry implements BaseColumns {
+        public static final String TABLE_NAME = "type";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_ICONURL = "iconurl";
+    }
+
+    private static final String SQL_CREATE_DEVICE_ENTRIES =
+            "CREATE TABLE " + DeviceEntry.TABLE_NAME + " (" +
+                    DeviceEntry._ID + " INTEGER PRIMARY KEY," +
+                    DeviceEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    DeviceEntry.COLUMN_NAME_DESCRIPTION + " TEXT," +
+                    DeviceEntry.COLUMN_NAME_PINNUMBER + " INTEGER," +
+                    DeviceEntry.COLUMN_NAME_TYPEID + " INTEGER)";
+
+
+    private static final String SQL_CREATE_TYPE_ENTRIES =
+            "CREATE TABLE " + TypeEntry.TABLE_NAME + " (" +
+                    TypeEntry._ID + " INTEGER PRIMARY KEY," +
+                    TypeEntry.COLUMN_NAME_NAME + " TEXT," +
+                    TypeEntry.COLUMN_NAME_DESCRIPTION + " TEXT," +
+                    TypeEntry.COLUMN_NAME_ICONURL + " TEXT)";
+
+    private static final String SQL_DELETE_DEVICE_ENTRIES =
+            "DROP TABLE IF EXISTS " + DeviceEntry.TABLE_NAME;
+
+    private static final String SQL_DELETE_TYPE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TypeEntry.TABLE_NAME;
+
+    public class ConnHomeDbHelper extends SQLiteOpenHelper {
+
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "ConnHome.db";
+
+        public ConnHomeDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_DEVICE_ENTRIES);
+            db.execSQL(SQL_CREATE_TYPE_ENTRIES);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(SQL_DELETE_DEVICE_ENTRIES);
+            db.execSQL(SQL_DELETE_TYPE_ENTRIES);
+            onCreate(db);
+        }
+    }
+}
---
+++ app/src/main/java/Model/ConnHomeDbHelper.java
@@ -0,0 +1,11 @@
+package Model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Jonathan on 23-05-2017.
+ */
+
+
